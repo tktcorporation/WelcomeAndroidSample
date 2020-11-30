a43741a557beacae3b72fd9687da3312a3d0c98c
tkt <tktcorporation.go@gmail.com>
2020-11-30 19:57:22 +0900

    set welcome screen theme

---
+++ app/src/main/java/com/tktcorporation/welcomeandroidsample/TutorialActivity.kt
@@ -0,0 +1,10 @@
+package com.tktcorporation.welcomeandroidsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
