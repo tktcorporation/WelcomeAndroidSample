eab56881ab0a85ee1beed646b658815a750369d1
tkt <tktcorporation.go@gmail.com>
2020-11-30 20:32:39 +0900

    implement

--- app/src/main/java/com/tktcorporation/welcomeandroidsample/MainActivity.kt
+++ app/src/main/java/com/tktcorporation/welcomeandroidsample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tktcorporation.welcomeandroidsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        TutorialActivity.showIfNeeded(this, null)
     }
 }
